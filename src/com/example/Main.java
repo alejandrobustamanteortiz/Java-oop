e2392422f8ad58933251cf4e06db62afdbd129ca
bububuu <albusor@gmail.com>
2023-01-17 13:12:32 +0100

    Suma, media y máximo

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -1,10 +1,32 @@
 package com.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("holaaa");
+        Product producto1 = new Product("Cola-Cao", 3.95, 00014502);
+        Product producto2 = new Product("Pan", 0.90, 0012445);
+        Product producto3 = new Product("Azucar", 1.55, 000123);
+        Product producto4 = new Product("Leche", 1.02, 00234);
+        Product producto5 = new Product("Galletas", 3.32, 00034);
+
+        Product[] cesta = {producto1, producto2, producto3, producto4, producto5};
+
+        double result = Product.calculateTotalPrice(cesta);
+        System.out.println("El precio total de su compra es: " + result);
+
+        double mediaPrices = Product.calculateMediaPrices(cesta);
+        System.out.println("La media de precios es: " + mediaPrices);
+
+
+        double maxProductPrice = Product.calculaMaxPrice(cesta);
+        System.out.println("El precio máximo es: " + maxProductPrice);
+
 
     }
+
 }
+
--- src/com/example/Product.java
+++ src/com/example/Product.java
@@ -1,4 +1,83 @@
 package com.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Product {
+
+    private String productName;
+    private double price;
+    private int productId;
+
+    public Product(){
+
+    }
+    public Product(String productName, double price, int productId){
+        this.productName = productName;
+        this.price = price;
+        this.productId = productId;
+    }
+
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+
+    public static double calculateTotalPrice(Product[] cesta){
+
+        double totalPrice = 0.0;
+
+        for (Product product : cesta) {
+            totalPrice = totalPrice + product.getPrice();
+        }
+        return totalPrice;
+    }
+
+    public static double  calculateMediaPrices(Product[] cesta){
+
+        return calculateTotalPrice(cesta) / cesta.length;
+    }
+
+    public static double calculaMaxPrice (Product[]cesta){
+
+        double maxPrice = 0.0;
+
+        for (int i = 0; i<cesta.length; i++) {
+
+            if( cesta[i].getPrice() > maxPrice) {
+                maxPrice = cesta[i].getPrice();
+            }
+        }
+        return maxPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "productName='" + productName + '\'' +
+                ", price=" + price +
+                ", productId=" + productId +
+                '}';
+    }
 }
